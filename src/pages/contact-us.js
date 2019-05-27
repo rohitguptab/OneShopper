495100648270a6a465640edede2ed80ab8756c57
Rohit Gupta <rohitguptab33@gmail.com>
2019-05-27 19:40:29 +0530

    added file changes

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -9,17 +9,26 @@ class Contact extends React.Component {
                 <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
                 <div className="Contact-us">
                     <div className="container">
-                        <form name="contact" netlify>
-                            <p>
-                                <label>Name <input type="text" name="name" /></label>
-                            </p>
-                            <p>
-                                <label>Email <input type="email" name="email" /></label>
-                            </p>
-                            <p>
-                                <button type="submit">Send</button>
-                            </p>
-                        </form>
+                    <form name="contact" method="POST" data-netlify="true">
+                        <p>
+                            <label>Your Name: <input type="text" name="name" /></label>   
+                        </p>
+                        <p>
+                            <label>Your Email: <input type="email" name="email" /></label>
+                        </p>
+                        <p>
+                            <label>Your Role: <select name="role[]" multiple>
+                            <option value="leader">Leader</option>
+                            <option value="follower">Follower</option>
+                            </select></label>
+                        </p>
+                        <p>
+                            <label>Message: <textarea name="message"></textarea></label>
+                        </p>
+                        <p>
+                            <button type="submit">Send</button>
+                        </p>
+                    </form>
                     </div>
                 </div>
             </ Layout>
